26b3787d06617dc5034e1132d4c667ec524c9c8f
jrunion <jwrunion@outlook.com>
2025-07-24 23:27:20 -0700

    initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.02
+
+@onready var head = $Face
+@onready var camera = $Face/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#inputs can be edited and changed under Project > Project Settings > Input Map
+	var input_dir := Input.get_vector("ui_cleft", "ui_cright", "ui_forward", "ui_backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = 0
+		velocity.z = 0
+
+	move_and_slide()
