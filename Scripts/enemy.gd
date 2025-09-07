d9b0ab74c3bcce4ecb3c66996a69b6c5769fa736
jrunion <jwrunion@outlook.com>
2025-09-06 21:33:09 -0700

    Started work on enemy states

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,6 +4,7 @@ var player = null
 
 
 const SPEED = 4
+const WEIGHT = 5
 
 @export var player_path : NodePath
 
@@ -17,12 +18,24 @@ func _ready():
 
 
 #called every frame
-func _process(_delta):
+func _process(delta):
 	velocity = Vector3.ZERO
 	nav_agent.set_target_position(player.global_transform.origin)
+
 	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	if detectedPlayer:
+		velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta * WEIGHT
+		
 	move_and_slide()
 	
-	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+
+#func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	
+		
 	
